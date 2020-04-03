032510ce0ff14a98a00223baa08cd8f8e22720e4
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-03 20:18:03 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,14 @@
 class Post 
   attr_accessor :name, :author
   @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @author = author 
+    save
+  end 
+  
+  def save
+    @@all << self
+  end 
 end 
